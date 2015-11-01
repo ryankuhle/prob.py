a9b086d054ce7917485cd8601fd5868697af4b1f
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-01 09:55:06 -0600

    Import needed libraries. Define data set

---
+++ prob.py
@@ -0,0 +1,8 @@
+import collections #needed to count numbers in set, for frequency
+import matplotlib.pyplot as plt #boxplot, histogram, qq
+#may need these for qq, not sure
+import numpy as np 
+import scipy.stats as stats
+
+#Initial Data set
+x = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 5, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 9, 9]
