dfad1c54f9bab9e5f065a73a49605c77e42ad7f8
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-01 10:16:19 -0600

    Create QQ chart from data set

--- prob.py
+++ prob.py
@@ -1,8 +1,6 @@
 import collections #needed to count numbers in set, for frequency
 import matplotlib.pyplot as plt #boxplot, histogram, qq
-#may need these for qq, not sure
-import numpy as np 
-import scipy.stats as stats
+import scipy.stats as stats #qq
 
 #Initial Data set
 x = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 5, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 9, 9]
@@ -27,4 +25,4 @@ for k,v in f.iteritems():
 #QQ
 plt.figure()
 qq = stats.probplot(x, dist="norm", plot=plt)
-plt.show()
+plt.savefig('qq.png')
